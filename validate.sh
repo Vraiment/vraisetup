835a7591160b0903ff1acb7bfc0cd1853dfcb0df
Vraiment <jemc44@gmail.com>
2023-09-24 17:51:40 -0700

    Add nativefier and WhatsApp

--- validate.sh
+++ validate.sh
@@ -4,5 +4,29 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -o pipefail # Propagate exit code on a pipeline
 set -x # Print commands and their arguments as they are executed.
 
-shellcheck validate.sh
-shellcheck bin/ubuntu.sh
+function main() {
+    ensure-shellcheck
+    validate-files
+}
+
+function ensure-shellcheck() {
+    if ! command -v shellcheck; then
+        >&2 echo shellcheck not found
+        exit 1
+    fi
+}
+
+function validate-files() {
+    local files
+
+    files=(
+        bin/ubuntu.sh
+        bin/nativefy.sh
+        validate.sh
+    )
+    readonly files
+
+    shellcheck "${files[@]}"
+}
+
+main "$@"
