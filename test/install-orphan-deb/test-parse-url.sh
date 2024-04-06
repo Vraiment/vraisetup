b987ce00854480ddf277e6b49e3f0ecf942289e6
Vraiment <jemc44@gmail.com>
2024-04-06 11:06:04 -0700

    Even more wip

--- test/install-orphan-deb/test-parse-url.sh
+++ test/install-orphan-deb/test-parse-url.sh
@@ -4,14 +4,11 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -o pipefail # Propagate exit code on a pipeline
 test -n "$VRAI_DEBUG" && set -x # Print commands and their arguments as they are executed.
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-readonly SCRIPT_DIR
-
-REPOSITORY_ROOT="$(cd -- "$SCRIPT_DIR"/../.. &> /dev/null && pwd)"
-readonly REPOSITORY_ROOT
+TEST_SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+readonly TEST_SCRIPT_DIR
 
 # shellcheck source=libexec/liborphandeb.sh
-source "$REPOSITORY_ROOT"/libexec/liborphandeb.sh
+source "$TEST_SCRIPT_DIR"/../../libexec/liborphandeb.sh
 
 function test-url-file-without-version() {
     local config_file
@@ -39,6 +36,3 @@ EOF
 
     test "$(parse-url)" = https://some-url.com/with/0.0.0/0.0.0.deb
 }
-
-# shellcheck source=test/run-tests.sh
-source "$SCRIPT_DIR"/../run-tests.sh
--- test/run-tests.sh
+++ test/run-tests.sh
@@ -4,17 +4,54 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -o pipefail # Propagate exit code on a pipeline
 test -n "$VRAI_DEBUG" && set -x # Print commands and their arguments as they are executed.
 
-while IFS= read -r __test_function; do
-    echo -e "\033[0;33mRunning test $__test_function...\033[0m"
+function on-a-separate-bash() {
+    local -r function_name="$1"
+    shift
 
-    if "$__test_function"; then
-        echo -e "\033[0;32m[SUCCEED] $__test_function\033[0m"
+    local var variables_to_forward
+    variables_to_forward=''
+    for var in "$@"; do
+        variables_to_forward+="$var=${!var} "
+    done
+
+    "$BASH" -c "$(declare -f "$function_name"); $variables_to_forward $function_name"
+}
+
+function read-tests() {
+    # shellcheck disable=SC1090
+    source "$test_file"
+
+    declare -F | cut --delimiter=' ' --field=3 | grep '^test-'
+}
+
+function run-single-test() {
+    echo -e "\033[0;33mRunning test $test_to_run...\033[0m"
+
+    # shellcheck disable=SC1090
+    source "$test_file"
+
+    if "$test_to_run"; then
+        echo -e "\033[0;32m[SUCCEED] $test_to_run\033[0m"
     else
-        __tests_failed=true
-        echo -e "\033[0;31m[FAILED] $__test_function\033[0m"
+        echo -e "\033[0;31m[FAILED] $test_to_run\033[0m"
     fi
-done < <(declare -F | cut --delimiter=' ' --field=3 | grep '^test-')
+}
 
-if [ "$__tests_failed" = true ]; then
-    exit 1
-fi
+function main() {
+    local -r test_file="$1"
+    local -a tests_to_run
+
+    readarray -t tests_to_run <<< "$(on-a-separate-bash read-tests test_file)"
+    # shellcheck disable=SC2128
+    if [ -z "$tests_to_run" ]; then
+        # If the array has an empty first element, then nothing was read
+        >&2 echo Failed to read files from "$test_file"
+        exit 1
+    fi
+
+    for test_to_run in "${tests_to_run[@]}"; do
+        on-a-separate-bash run-single-test test_file test_to_run
+    done
+}
+
+main "$@"
