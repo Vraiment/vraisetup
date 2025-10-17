06adddcab7284e3a74cf2fdeea647f57ff0d943f
Vraiment <jemc44@gmail.com>
2025-10-16 23:54:01 -0700

    Move orphan software installation to be Ansible managed

--- bin/ubuntu.sh
+++ bin/ubuntu.sh
@@ -17,8 +17,6 @@ function main() {
 
     /usr/bin/mkdir --parents "$data_dir"
 
-    install-orphan-software
-
     install-scripting-runtimes
 
     # Enable "sudoless" `docker`
@@ -41,101 +39,6 @@ function ensure-sudo() {
     # this will make it so later calls will have credentials figured out
     /usr/bin/sudo --remove-timestamp
     /usr/bin/sudo /usr/bin/true
-}
-
-# "Orphan" software in this context referts to applications that are not
-# *officially* distributed via `apt`, `flatpak`  or `snap` and need to
-# be downloaded and installed manually.
-function install-orphan-software() {
-    local deb_dir simplenote_version simplenote_deb steam_deb
-
-    deb_dir="$data_dir"/deb
-    readonly deb_dir
-
-    /usr/bin/mkdir --parents "$deb_dir"
-
-    # Install NAPS2, sha256sum calculated Sep 01st, 2025
-    # NAPS2 it's also offered as a Flatpak but given the USB limitations with Flatpak
-    # that version is literally useless.
-    naps2_version=8.2.1
-    readonly naps2_version
-    install-orphan-deb \
-        https://github.com/cyanfish/naps2/releases/download/v"$naps2_version"/naps2-"$naps2_version"-linux-x64.deb \
-        8e23c4a6d7f0b75512459cc29a639ec395355f222375291ed674525c7c9cd4b7 \
-        "$deb_dir"/naps2-"$naps2_version"-linux-x64.deb
-
-    # Install Simplenote, sha256sum calculated Jun 02nd, 2025
-    simplenote_version=2.23.2
-    simplenote_deb=Simplenote-linux-"$simplenote_version"-amd64.deb
-    readonly simplenote_version
-    install-orphan-deb \
-        https://github.com/Automattic/simplenote-electron/releases/download/v"$simplenote_version"/"$simplenote_deb" \
-        ae5c5a46347d68031324633c27f33afdeabfc6ffca5f58dd638002e6db99f22b \
-        "$deb_dir"/"$simplenote_deb"
-
-    # Install Steam, sha256sum calculated Sept 2nd, 2024
-    # Valve publishes the deb to the Steam page as `steam_latest.deb`
-    # but the repository is browsable and has links to all of the
-    # versions
-    steam_deb=steam-launcher_1.0.0.81_all.deb
-    readonly steam_deb
-    install-orphan-deb \
-        https://repo.steampowered.com/steam/archive/stable/"$steam_deb" \
-        afd2b922f9771a9ca7ee0cb416bdd4fceabf3e75a2c5b65f654a10419762960d \
-        "$deb_dir"/"$steam_deb"
-}
-
-function install-orphan-deb() {
-    local deb_url deb_sum deb_path tmp package_name package_new_version package_old_version
-
-    deb_url="$1"
-    readonly deb_url
-
-    deb_sum="$2"
-    readonly deb_sum
-
-    deb_path="$3"
-    readonly deb_path
-
-    # If "$deb_path" already exists, warranty it has a valid signature,
-    # otherwise back it up for future inspection and remove it.
-    if [ -e "$deb_path" ]; then
-        if ! echo "$deb_sum" "$deb_path" | /usr/bin/sha256sum --check; then
-            >&2 echo Suspicious deb "$deb_path", will back it up
-
-            # Convoluted way of moving `"$deb_path"` "into itself" so we can
-            # leverage `mv --backup` for backing it up.
-            # 1. Create a temporary file
-            # 2. Move the temporary file into the file we
-            #    want to backup
-            # 3. Delete the temporary file
-            tmp="$(mktemp)"
-            readonly tmp
-
-            /usr/bin/mv --backup=numbered "$tmp" "$deb_path"
-            /usr/bin/rm "$deb_path"
-        fi
-    fi
-
-    # If the deb didn't already exist with a valid signature, download it.
-    if [ ! -e "$deb_path" ]; then
-        /usr/bin/curl --output "$deb_path" --location "$deb_url"
-        echo "$deb_sum" "$deb_path" | /usr/bin/sha256sum --check
-    fi
-
-    package_name="$(/usr/bin/dpkg --field "$deb_path" Package)"
-    readonly package_name
-
-    package_new_version="$(/usr/bin/dpkg --field "$deb_path" Version)"
-    readonly package_new_version
-
-    package_old_version=$(/usr/bin/dpkg-query --showformat='${Version}' --show "$package_name" || echo '')
-    readonly package_old_version
-
-    # An empty "$package_old_version" means the package hasn't been installed
-    if [[ -z "$package_old_version" ]] || /usr/bin/dpkg --compare-versions "$package_new_version" gt "$package_old_version"; then
-        /usr/bin/sudo /usr/bin/apt install --assume-yes "$deb_path"
-    fi
 }
 
 function install-scripting-runtimes() {
