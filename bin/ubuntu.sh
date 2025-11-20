319241b38dd8d03a687cd76b496e6e9f9ebdcda4
Vraiment <jemc44@gmail.com>
2025-11-20 00:02:07 -0800

    Move AppImages setup to be Ansible managed

--- bin/ubuntu.sh
+++ bin/ubuntu.sh
@@ -21,8 +21,6 @@ function main() {
     /usr/bin/sudo /usr/sbin/groupadd --force docker
     /usr/bin/sudo /usr/sbin/usermod --append --groups docker "$USER"
 
-    setup-appimages
-
     "$setup_root"/applications/vscode/install.sh
 }
 
@@ -31,24 +29,6 @@ function ensure-sudo() {
     # this will make it so later calls will have credentials figured out
     /usr/bin/sudo --remove-timestamp
     /usr/bin/sudo /usr/bin/true
-}
-
-function setup-appimages() {
-    # Allows AppImages to be placed under `$HOME/Applications/*.AppImage`
-    /usr/bin/cat << EOF | /usr/bin/sudo /usr/bin/tee /etc/apparmor.d/userappimages
-abi <abi/4.0>,
-include <tunables/global>
-
-profile userappimages @{HOME}/Applications/*.AppImage flags=(default_allow) {
-  userns,
-
-  # Site-specific additions and overrides. See local/README for details.
-  include if exists <local/userappimages>
-}
-EOF
-    /usr/bin/sudo /usr/bin/systemctl reload apparmor
-
-    # TODO: Add `appimaged`
 }
 
 main "$@"
